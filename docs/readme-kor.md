3d463f9ec552fd79c416dac4dd7f6140b759bed5
Park DongHa <luncliff@gmail.com>
2020-01-01 21:16:40 +0900

    setup: migrated scripts for build envs
    use Travis CI release provider for GitHub Pages
    
    * mkdocs from ubuntu bionic
    * install clang-8 (even if it is not necessary)

---
+++ docs/readme-kor.md
@@ -0,0 +1,7 @@
+# Modern C++ Next
+
+빠른 C++ 20 입문
+
+```cpp
+const char* greetings = "hello C++ 20!";
+```
---
+++ scripts/setup-compilers-ubuntu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+#   Author  : github.com/luncliff (luncliff@gmail.com)
+#   References:
+#       https://apt.llvm.org/
+#
+verion_name=$(. /etc/os-release;echo $VERSION)
+distribution=$(. /etc/os-release;echo $UBUNTU_CODENAME)
+
+echo "----------------------------------------------------------------------"
+echo "                                                                      "
+echo " Install C++ Compilers: ${verion_name}                                "
+echo "  - Path      : /usr/bin                                              "
+echo "  - Compiler  : g++-8, g++-9, clang-8, clang-9                        "
+echo "                                                                      "
+echo "----------------------------------------------------------------------"
+
+# display release info. this will helpful for checking CI environment
+cat /etc/os-release
+
+apt update -qq
+apt install -y -qq \
+  build-essential software-properties-common
+apt-add-repository -y ppa:ubuntu-toolchain-r/test
+
+# http://apt.llvm.org/
+# use clang for 'stable, development branch'
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+apt-add-repository "deb http://apt.llvm.org/${distribution}/ llvm-toolchain-${distribution}-8 main"
+apt-add-repository "deb http://apt.llvm.org/${distribution}/ llvm-toolchain-${distribution}-9 main"
+
+apt update -qq
+apt install -y -q \
+  g++-8 g++-9 clang-8 clang-9 \
+  libc++abi-8-dev
+
+pushd /usr/bin
+ln -s --force gcc-9 gcc
+ln -s --force g++-9 g++
+ln -s --force clang-8 clang
+popd
